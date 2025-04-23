483bfd58dbbf14b6a73fbb7d5306353a80214115
Kenneth Lausdahl <kenneth@lausdahl.com>
2025-04-23 18:34:12 +0200

    added option to select layers on map

--- src/isoxmlviz/webmap.py
+++ src/isoxmlviz/webmap.py
@@ -52,6 +52,112 @@ class WebMap:
             max_zoom=22
         ).add_to(self.m)
 
+        # Add custom basemaps to folium
+        basemaps = {
+            'Google Maps': folium.TileLayer(
+                tiles='https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Maps',
+                overlay=True,
+                control=True
+            ),
+            'Google Satellite': folium.TileLayer(
+                tiles='https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Satellite',
+                overlay=True,
+                control=True
+            ),
+            'Google Terrain': folium.TileLayer(
+                tiles='https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Terrain',
+                overlay=True,
+                control=True
+            ),
+            'Google Satellite Hybrid': folium.TileLayer(
+                tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Satellite',
+                overlay=True,
+                control=True
+            ),
+            'Esri Satellite': folium.TileLayer(
+                tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+                attr='Esri',
+                name='Esri Satellite',
+                overlay=True,
+                control=True
+            )
+        }
+
+        def add_ee_layer(self, ee_object, vis_params, name):
+
+            try:
+                # display ee.Image()
+                if isinstance(ee_object, ee.image.Image):
+                    map_id_dict = ee.Image(ee_object).getMapId(vis_params)
+                    folium.raster_layers.TileLayer(
+                        tiles=map_id_dict['tile_fetcher'].url_format,
+                        attr='Google Earth Engine',
+                        name=name,
+                        overlay=True,
+                        control=True
+                    ).add_to(self)
+                # display ee.ImageCollection()
+                elif isinstance(ee_object, ee.imagecollection.ImageCollection):
+                    ee_object_new = ee_object.mosaic()
+                    map_id_dict = ee.Image(ee_object_new).getMapId(vis_params)
+                    folium.raster_layers.TileLayer(
+                        tiles=map_id_dict['tile_fetcher'].url_format,
+                        attr='Google Earth Engine',
+                        name=name,
+                        overlay=True,
+                        control=True
+                    ).add_to(self)
+                # display ee.Geometry()
+                elif isinstance(ee_object, ee.geometry.Geometry):
+                    folium.GeoJson(
+                        data=ee_object.getInfo(),
+                        name=name,
+                        overlay=True,
+                        control=True
+                    ).add_to(self)
+                # display ee.FeatureCollection()
+                elif isinstance(ee_object, ee.featurecollection.FeatureCollection):
+                    ee_object_new = ee.Image().paint(ee_object, 0, 2)
+                    map_id_dict = ee.Image(ee_object_new).getMapId(vis_params)
+                    folium.raster_layers.TileLayer(
+                        tiles=map_id_dict['tile_fetcher'].url_format,
+                        attr='Google Earth Engine',
+                        name=name,
+                        overlay=True,
+                        control=True
+                    ).add_to(self)
+
+            except:
+                print("Could not display {}".format(name))
+
+        # Add EE drawing method to folium.
+        # folium.Map.add_ee_layer = add_ee_layer
+
+        vis_params = {
+            'min': 0,
+            'max': 4000,
+            'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
+
+        # Create a folium map object.
+        my_map = folium.Map(location=[20, 0], zoom_start=3, height=500)
+
+        # Add custom basemaps
+        basemaps['Google Maps'].add_to(self.m)
+        basemaps['Google Satellite Hybrid'].add_to(self.m)
+
+        # Add the elevation model to the map object.
+        # self.m.add_ee_layer(dem.updateMask(dem.gt(0)), vis_params, 'DEM')
+
+
+
     def set_refernce(self, ref_lat, ref_lng):
         self.ref_lat = ref_lat
         self.ref_lng = ref_lng
